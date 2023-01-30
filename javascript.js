6f0c76712ef34ab99bb1b26ebbeb01af706a395d
Bryan <118778494+muffiez@users.noreply.github.com>
2023-01-30 16:08:40 -0500

    Create another function to loop through each round.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
-const choiceArr = ['Rock', 'Paper', 'Scissors'];
+let computerScore = 0;
+let playerScore = 0;
+let round = 0;
+
+const choiceArr = ['rock', 'paper', 'scissors'];
 
 let getComputerChoice = () => {
     index = randomNum(0, choiceArr.length-1)
@@ -9,31 +13,63 @@ function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const compChoice = getComputerChoice();
 
 
-let playerSelection = prompt('Please enter your desired weapon: Rock, Paper, or Scissors');
 
-console.log(`The computer chose ${compChoice}`);
 
-let game = (playerSelection, compChoice) => {
+
+// console.log(`The computer chose ${computerSelection}`);
+
+let playRound = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.toLowerCase();
-    console.log(`You chose ${playerSelection}`)
-    if ((playerSelection === 'rock' && compChoice === 'Rock') || (playerSelection === 'paper' && compChoice === 'Paper') || (playerSelection === 'scissors' && compChoice === 'Scissors')) {
-       return `Tie game! Both players chose ${capitalize(playerSelection)}!`;
+    // console.log(`You chose ${playerSelection}`)
+    if ((playerSelection === computerSelection)) {
+        return 'T';
     }
-    else if ((playerSelection === 'rock' && compChoice === 'Scissors') || (playerSelection === 'paper' && compChoice === 'Rock') || (playerSelection === 'scissors' && compChoice === 'Paper')) {
-        return `You win! ${capitalize(playerSelection)} beats ${compChoice}!`;
+    else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+    
+        return 'W';
     }
-    else if ((playerSelection === 'rock' && compChoice === 'Paper') || (playerSelection === 'paper' && compChoice === 'Scissors') || (playerSelection === 'scissors' && compChoice === 'Rock')) {
-        return `You Lose! ${compChoice} beats ${capitalize(playerSelection)}!`;
+    else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+       
+        return 'L';
     }
 }
 
-function capitalize(playerSelection) {
-    const firstLetter = playerSelection.substr(0,1);
-    const restOfString = playerSelection.substr(1);
-    return (firstLetter.toUpperCase() + restOfString.toLowerCase());
+function capitalize(str) {
+    return str.substr(0,1).toUpperCase() + str.slice(1);
+
 }
 
-console.log(game(playerSelection, compChoice));
+// console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+
+    
+   for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt('Please enter your desired weapon: Rock, Paper, or Scissors');
+    const computerSelection = getComputerChoice();    
+   const result = playRound(playerSelection, computerSelection)
+       
+            if(result === 'W') {
+                console.log(`Round: ${round += 1} Player: ${playerScore +=1 } Computer: ${computerScore} You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}!`);
+            
+               
+            }
+            else if(result === 'L') {
+                console.log(`Round: ${round +=1} Player: ${playerScore} Computer: ${computerScore +=1} You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}!`);
+                
+               
+            }
+            else if(result === 'T') {
+                console.log(`Round: ${round} Player: ${playerScore} Computer: ${computerScore} Tie game! Both players chose ${capitalize(playerSelection)}! Replay the round.`);
+                i--;
+            }
+            else {
+                console.log("an error occured");
+                i--;
+            }
+        
+    }
+}
+game();

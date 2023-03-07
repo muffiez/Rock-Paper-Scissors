14fb9c1d594665049e6b030866b3211127e6ed77
Bryan <118778494+muffiez@users.noreply.github.com>
2023-03-07 17:10:28 -0500

    Add basic css to game.

--- javascript.js
+++ javascript.js
@@ -18,27 +18,23 @@ const roundInfo = document.querySelector('#round');
 const playerScoreInfo = document.querySelector('#player');
 const computerScoreInfo = document.querySelector('#computer');
 const message = document.querySelector('#message');
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('div.choices');
+
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         const playerSelection = e.target.id;
-        
         const computerSelection = getComputerChoice();
-        
         playRound(playerSelection, computerSelection);
-        
         if (playerScore >= 5 || computerScore >=5 ) {
-           
+            removeButtons();
             endMessage();
             document.querySelector('#replay').removeAttribute('hidden');
         }
-
     });
 });
 
 let playRound = (playerSelection, computerSelection) => {
-//    computerSelection = getComputerChoice();
     if ((playerSelection === computerSelection)) {
         round += 1;
         roundWinner = 'tie'
@@ -57,25 +53,27 @@ let playRound = (playerSelection, computerSelection) => {
         computerScore += 1;
         roundWinner = 'computer'
     }
-    roundMessage(roundWinner, playerSelection, computerSelection);
-       roundInfo.textContent = `Round ${round}`;
-       playerScoreInfo.textContent = `Player ${playerScore}`;
-       computerScoreInfo.textContent = `Computer ${computerScore}`;
-      
+    displayRoundMessage(roundWinner, playerSelection, computerSelection);
+    roundInfo.textContent = `Round ${round}`;
+    playerScoreInfo.textContent = `Player ${playerScore}`;
+    computerScoreInfo.textContent = `Computer ${computerScore}`;   
 }
 
-let roundMessage = (roundWinner, playerSelection, computerSelection) => {
+let displayRoundMessage = (roundWinner, playerSelection, computerSelection) => {
     if (roundWinner === 'player') {
-        message.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
+        message.textContent = `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}!`;
     }
     else if (roundWinner === 'computer') {
-        message.textContent = `You Lose! ${computerSelection} beats ${playerSelection}!`;
+        message.textContent = `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}!`;
     }
     else {
-        message.textContent = `Tie game! Both players chose ${playerSelection}! Replay the round.`;
+        message.textContent = `Tie game! Both players chose ${capitalize(playerSelection)}! Replay the round.`;
     }
 }
 
+let capitalize = (str) => {
+    return str.substr(0,1).toUpperCase() + str.slice(1);
+}
 
 let endMessage = () => {
     if (playerScore > computerScore) {
@@ -86,19 +84,24 @@ let endMessage = () => {
     }
 }
 
+let removeButtons = () => {
+    document.getElementById('rock').style.display = 'none';
+    document.getElementById('paper').style.display = 'none';
+    document.getElementById('scissors').style.display = 'none';
+}
 
-
-    
-const onReplay = (e) => {
-
-document.querySelector('#round').innerHTML = '';
-document.querySelector('#player').innerHTML = '';
-document.querySelector('#computer').innerHTML = '';
-document.querySelector('#message').innerHTML = '';
-document.querySelector('#end').innerHTML = '';
-computerScore = 0;
-playerScore = 0;
-round = 0;
-roundWinner = '';
-e.target.setAttribute('hidden', 'hidden');
+let onReplay = (e) => {
+    document.querySelector('#round').innerHTML = '';
+    document.querySelector('#player').innerHTML = '';
+    document.querySelector('#computer').innerHTML = '';
+    document.querySelector('#message').innerHTML = '';
+    document.querySelector('#end').innerHTML = '';
+    computerScore = 0;
+    playerScore = 0;
+    round = 0;
+    roundWinner = '';
+    e.target.setAttribute('hidden', 'hidden');
+    document.getElementById('rock').style.display = 'block';
+    document.getElementById('paper').style.display = 'block';
+    document.getElementById('scissors').style.display = 'block';
 }
